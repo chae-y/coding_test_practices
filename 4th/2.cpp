4693a528e5753b457a32ac589c2ac2d9e32eb2ae
chae09945ss <stat_chae0@sookmyung.ac.kr>
2020-12-30 20:46:16 +0900

    수학적 귀납법-k진법

---
+++ 4th/2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int f(int n, int k) {
+	if (n < k) {
+		cout << n;
+		return 0;
+	}
+	f(n / k, k);
+	cout << (n % k);
+}
+
+int main() {
+	int n,k;
+	cin >> n >> k;
+	f(n, k);
+	return 0;
+}
