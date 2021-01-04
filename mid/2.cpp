154d0c92c52e524b6517389bc68c7b25cb9d1c43
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-01-04 23:51:46 +0900

    하는중

---
+++ mid/2.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct work{
+	int s;
+	int e;
+	int w;
+};
+vector<work> v;
+
+bool sorting(work w1, work w2) {
+	if (w1.s == w2.s)	return w1.e < w2.e;
+	return w1.s < w2.s;
+}
+
+bool solution(int t){
+	sort(v.begin(),v.end(), sorting);
+	int day = 1;
+	int time = 1;
+	for (int i = 0; i < t; i++) {
+		cout << v[i].s << v[i].e << v[i].w<<endl;
+		if (day <= v[i].s) {
+			if (day > v[i].e)	return false;
+			while (v[i].w > 0) {
+				time++;
+				v[i].w--;
+				cout << i << " ";
+				if (time == 9) {
+					if (day > v[i].e)	return false;
+					day++; time = 1;
+				}
+			}
+			cout << endl;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int T;
+		cin >> T;
+		for (int j = 0; j < T; j++) {
+			int s, e, w;
+			cin >> s >> e >> w;
+			work work;
+			work.s = s; work.e = e; work.w = w;
+			v.push_back(work);
+		}
+		if (solution(T))	cout << "yes"<<endl;
+		else cout << "no"<<endl;
+
+	}
+	return 0;
+}
