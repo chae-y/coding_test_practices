e37abe6d55f470d93d505db9a324f3bf075632fe
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-03-11 23:58:20 +0900

    no message

--- 11st/pro_2.cpp
+++ 11st/pro_2.cpp
@@ -1,4 +1,4 @@
-// 다음과 같이 include를 사용할 수 있습니다.
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,7 +6,6 @@
 using namespace std;
 
 int solution(vector<vector<int>> garden) {
-    // 여기에 코드를 작성해주세요.
     int answer = 0;
     vector<pair<int, int>> v;
     int dx[4] = { 0,0,-1,1 };
@@ -39,17 +38,11 @@ int solution(vector<vector<int>> garden) {
     return answer;
 }
 
-// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
 int main() {
     vector<vector<int>> garden1 = { {0, 0, 0}, {0, 1, 0}, {0, 0, 0} };
     int ret1 = solution(garden1);
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     cout << "solution 함수의 반환 값은 " << ret1 << " 입니다." << endl;
 
     vector<vector<int>> garden2 = { {1, 1}, {1, 1} };
-    //int ret2 = solution(garden2);
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    //cout << "solution 함수의 반환 값은 " << ret2 << " 입니다." << endl;
 }
