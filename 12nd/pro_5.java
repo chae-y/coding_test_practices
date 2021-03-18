9a9c33e271dae17635000632aab39d4a9dddf427
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-03-18 18:17:18 +0900

    조이스틱

---
+++ 12nd/pro_5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        int min_move=len-1;
+
+        for(int i=0; i<len; i++){
+            if(name.charAt(i)<'N'){
+                answer+= name.charAt(i)-'A';
+            }else{
+                answer+= 'Z'-name.charAt(i)+1;
+            }
+            int next=i+1;
+            while(next<len&&name.charAt(next)=='A'){
+                next++;
+            }
+            min_move = Math.min(min_move, i+len-next+Math.min(i,len-next));
+        }
+        answer += min_move;
+            return answer;
+    }
+}
