3206458329692fefe65556b8cb3f34d2a5688a08
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-04-08 13:42:14 +0900

    폰켓몬

---
+++ sort/pro_4.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int size=nums.size()/2;
+    sort(nums.begin(),nums.end());
+    nums.erase(unique(nums.begin(),nums.end()),nums.end());
+    int uni=nums.size();
+    printf("%d",uni);
+    if(size>uni)    answer=uni;
+    else answer=size;
+    return answer;
+}
