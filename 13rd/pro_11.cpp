b9792d2983c6c7fc9333f3d6fa98603d38f25acb
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-04-09 13:44:05 +0900

    올바른 괄호

---
+++ 13rd/pro_11.cpp
@@ -0,0 +1,20 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int tmp=0;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='(') tmp++;
+        if(s[i]==')') tmp--;
+        if(tmp<0){
+            answer=false;
+            break;
+        }
+    }
+    if(tmp!=0) answer=false;
+    return answer;
+}
