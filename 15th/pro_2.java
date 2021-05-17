7d8059713e3a9a7d9f744e244b4b5b78b3b1483c
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-05-17 17:00:19 +0900

    예상대진표

---
+++ 15th/pro_2.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        while(true){
+            if(a%2==0&&a==b+1)   break;
+            if(a%2==1&&a==b-1)  break;
+            a=(a+1)/2;
+            b=(b+1)/2;
+            answer++;
+        }
+        return answer;
+    }
+}
